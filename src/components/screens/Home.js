d60f778221209b6dbaebc5738bc629f8c7b57f30
trang thuan kiet <thuankiet12@gmail.com>
2020-06-30 01:08:29 +0700

    add Home and Profile Screen

--- src/components/screens/Home.js
+++ src/components/screens/Home.js
@@ -1,7 +1,57 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <div className="card home-card">
+        <h5>Thuan Kiet</h5>
+        <div className="card-image">
+          <img
+            alt="cardimage"
+            src="https://images.unsplash.com/photo-1508739773434-c26b3d09e071?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+          />
+        </div>
+        <div className="card-content">
+          <i class="material-icons">favorite</i>
+          <h6>title</h6>
+          <p>this is an amazing post</p>
+          <input type="text" placeholder="add a comment" />
+        </div>
+      </div>
+
+      <div className="card home-card">
+        <h5>Thuan Kiet</h5>
+        <div className="card-image">
+          <img
+            alt="cardimage"
+            src="https://images.unsplash.com/photo-1508739773434-c26b3d09e071?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+          />
+        </div>
+        <div className="card-content">
+          <i class="material-icons">favorite</i>
+          <h6>title</h6>
+          <p>this is an amazing post</p>
+          <input type="text" placeholder="add a comment" />
+        </div>
+      </div>
+
+      <div className="card home-card">
+        <h5>Thuan Kiet</h5>
+        <div className="card-image">
+          <img
+            alt="cardimage"
+            src="https://images.unsplash.com/photo-1508739773434-c26b3d09e071?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+          />
+        </div>
+        <div className="card-content">
+          <i class="material-icons">favorite</i>
+          <h6>title</h6>
+          <p>this is an amazing post</p>
+          <input type="text" placeholder="add a comment" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/components/screens/Profile.js
+++ src/components/screens/Profile.js
@@ -1,7 +1,74 @@
 import React from "react";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <div style={{ maxWidth: "550px", margin: "0 auto" }}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-around",
+          margin: "18px 0",
+          borderBottom: "1px solid grey",
+        }}
+      >
+        <div>
+          <img
+            style={{ width: "160px", height: "160px", borderRadius: "50%" }}
+            src="https://images.unsplash.com/photo-1557037559-bedd47b49aaf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+            alt="profile"
+          />
+        </div>
+
+        <div>
+          <h4>Thuan Kiet</h4>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-around",
+              width: "108%",
+            }}
+          >
+            <h6>40 posts</h6>
+            <h6>40 followers</h6>
+            <h6>40 following</h6>
+          </div>
+        </div>
+      </div>
+
+      <div className="gallary">
+        <img
+          className="item"
+          src="https://images.unsplash.com/photo-1557037559-bedd47b49aaf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+          alt="profile"
+        />
+        <img
+          className="item"
+          src="https://images.unsplash.com/photo-1557037559-bedd47b49aaf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+          alt="profile"
+        />
+        <img
+          className="item"
+          src="https://images.unsplash.com/photo-1557037559-bedd47b49aaf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+          alt="profile"
+        />
+        <img
+          className="item"
+          src="https://images.unsplash.com/photo-1557037559-bedd47b49aaf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+          alt="profile"
+        />
+        <img
+          className="item"
+          src="https://images.unsplash.com/photo-1557037559-bedd47b49aaf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+          alt="profile"
+        />
+        <img
+          className="item"
+          src="https://images.unsplash.com/photo-1557037559-bedd47b49aaf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+          alt="profile"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
