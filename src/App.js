2be34a66788414c125b9d743e49ca13e7efdfb46
trang thuan kiet <thuankiet12@gmail.com>
2020-06-29 21:29:06 +0700

    add React Router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./components/screens/Home";
+import Signin from "./components/screens/Login";
+import Profile from "./components/screens/Profile";
+import Signup from "./components/screens/Signup";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Route path="/" exact>
+        <Home />
+      </Route>
+      <Route path="/signin">
+        <Signin />
+      </Route>
+      <Route path="/signup">
+        <Signup />
+      </Route>
+      <Route path="/profile">
+        <Profile />
+      </Route>
+    </Router>
   );
 }
 
